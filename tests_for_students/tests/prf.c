7257fd52590fba9abaf8e654afad5bc938d85d79
nabilkh1 <nabil.khoury@campus.technion.ac.il>
2026-07-05 20:10:10 +0300

    changing approach 6

--- tests_for_students/tests/prf.c
+++ tests_for_students/tests/prf.c
@@ -178,15 +178,12 @@ void run_tracer(pid_t child_pid, unsigned long addr, int nr_params)
         } else if(orig_return_addr != 0 && regs.rip == orig_return_addr) {
             printf("PRF::   call to function returned with %llu\n", regs.rax);
             ptrace(PTRACE_POKETEXT, child_pid, (void*)orig_return_addr, (void*)ret_data);
+            ptrace(PTRACE_CONT, child_pid, NULL, NULL);
+            wait(&wait_status);
             orig_return_addr = 0;
             ret_data = 0;
-            ret_data_trap = 0;
             call_depth = 0;
-            ptrace(PTRACE_SINGLESTEP, child_pid, NULL, NULL);
-            wait(&wait_status);
-            ptrace(PTRACE_CONT, child_pid, NULL, NULL);
         }
-        wait(&wait_status);
     }
 }
 
