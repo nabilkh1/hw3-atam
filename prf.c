c673b660a80da971b04d62873a7e9f54401b5b67
nabilkh1 <nabil.khoury@campus.technion.ac.il>
2026-07-05 16:34:13 +0300

    exiting trace

--- prf.c
+++ prf.c
@@ -176,7 +176,8 @@ void run_tracer(pid_t child_pid, unsigned long addr, int nr_params)
         } else if(orig_return_addr != 0 && regs.rip == orig_return_addr) {
             printf("PRF::  call function returned with %llu\n", regs.rax);
             ptrace(PTRACE_POKETEXT, child_pid, (void*)orig_return_addr, (void*)ret_data);
-            break;
+            ptrace(PTRACE_CONT, child_pid, NULL, NULL);
+            wait(&wait_status);
         }
     }
 }
