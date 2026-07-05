0edcefbcf55060409c1329968a9ce148737c9c70
nabilkh1 <nabil.khoury@campus.technion.ac.il>
2026-07-05 16:30:48 +0300

    done

---
+++ prf.c
@@ -0,0 +1,219 @@
+#include <assert.h>
+#include <elf.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ptrace.h>
+#include <sys/reg.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/user.h>
+#include <sys/wait.h>
+#include <syscall.h>
+#include <unistd.h>
+
+unsigned long func_size;
+
+/*
+ * Fork a child process and set it up for tracing.
+ * Replaces the child's image with the target program.
+ */
+pid_t run_target(char* const argv[])
+{
+    pid_t pid = fork();
+    if (pid > 0) {
+        return pid;
+    } else if (pid == 0) {
+        if (ptrace(PTRACE_TRACEME, 0, NULL, NULL) < 0) {
+            perror("ptrace");
+            exit(1);
+        }
+        execv(argv[0], argv);
+        exit(1);
+    } else {
+        perror("fork");
+        exit(1);
+    }
+}
+
+void* get_elf_content(const char* sym_name, const char* file_name)
+{
+    // Open ELF file for reading
+    int elf_fd = open(file_name, O_RDONLY);
+    if (elf_fd < 0) {
+        perror("failed to open file");
+        return NULL;
+    }
+
+    // Get file size
+    struct stat elf_stats;
+    int ret = fstat(elf_fd, &elf_stats);
+    if (ret < 0) {
+        perror("fstat failed");
+        close(elf_fd);
+        return NULL;
+    }
+    long file_size = elf_stats.st_size;
+
+    // Allocate memory for the file content
+    void* file_content = malloc(file_size);
+    if (!file_content) {
+        perror("malloc failed");
+        close(elf_fd);
+        return NULL;
+    }
+
+    // Read ELF file content into memory
+    ret = read(elf_fd, file_content, file_size);
+    if (ret < file_size) {
+        perror("read failed or incomplete");
+        free(file_content);
+        close(elf_fd);
+        return NULL;
+    }
+    close(elf_fd);
+
+    return file_content;
+}
+
+unsigned long parse_elf(const char* target_sym, void* file_contents)
+{
+    // TODO: Look up target_sym address in the elf binary and return its virtual
+    Elf64_Ehdr *ehdr = file_contents;
+    Elf64_Shdr *shdr = file_contents + ehdr->e_shoff;
+    int symtab_idx = -1;
+    for (int i = 0; i < ehdr->e_shnum; i++) {
+        if (shdr[i].sh_type == SHT_SYMTAB) {
+            symtab_idx = i;
+            break;
+        }
+    }
+    if (symtab_idx == -1) return 0;
+    Elf64_Sym *syms = file_contents + shdr[symtab_idx].sh_offset;
+    int strtab_idx = shdr[symtab_idx].sh_link;
+    char *strtab = file_contents + shdr[strtab_idx].sh_offset;
+    int num_symbols = shdr[symtab_idx].sh_size / sizeof(Elf64_Sym);
+    for (int i = 0; i < num_symbols; i++) {
+        char *current_sym_name = strtab + syms[i].st_name;
+        if (strcmp(current_sym_name, target_sym) == 0) {
+            func_size = syms[i].st_size;
+            return syms[i].st_value;
+        }
+    }
+    // address. Return 0 if not found.
+}
+
+/*
+ * Main debugger tracing loop.
+ */
+void run_tracer(pid_t child_pid, unsigned long addr, int nr_params)
+{
+    int wait_status;
+    int run_idx = 1;
+    struct user_regs_struct regs;
+
+    wait(&wait_status);
+
+    // TODO: Implement tracing logic
+    unsigned long data = ptrace(PTRACE_PEEKTEXT, child_pid, (void*)addr, NULL);
+    unsigned char first_byte = data & 0xFF;
+    if (first_byte == 0x55) {
+        printf("PRF:: This function starts by pushing rbp\n");
+    }
+
+    unsigned long data_trap = (data & 0xFFFFFFFFFFFFFF00) | 0xCC;
+    ptrace(PTRACE_POKETEXT, child_pid, (void*)addr, (void*)data_trap);
+    ptrace(PTRACE_CONT, child_pid, NULL, NULL);
+    wait(&wait_status);
+
+    unsigned long orig_return_addr = 0;
+    unsigned long ret_data = 0;
+    unsigned long ret_data_trap = 0;
+    while(WIFSTOPPED(wait_status)) {
+        ptrace(PTRACE_GETREGS, child_pid, NULL, &regs);
+        regs.rip -= 1;
+        ptrace(PTRACE_SETREGS, child_pid, NULL, &regs);
+        if (regs.rip == addr) {
+            unsigned long return_addr = ptrace(PTRACE_PEEKTEXT, child_pid, (void*)regs.rsp, NULL);
+            unsigned long long param_regs[6] = {regs.rdi, regs.rsi, regs.rdx, regs.rcx, regs.r8, regs.r9}; // first 6 params are passed in registers
+        if (return_addr >= addr && return_addr < addr + func_size) { // check if its a recursive call
+            printf("PRF::    entered recursive call with (");
+            if (nr_params > 0) {
+             for (int i=0; i<nr_params-1; i++) {
+                printf("%llu, ",param_regs[i]);
+            }
+            printf("%llu)\n",param_regs[nr_params-1]);
+            } else {
+                printf(")\n");
+            }
+
+        } else { // normal call 
+            printf("PRF:: run #%d called with (", run_idx);
+            if (nr_params > 0) {
+                for (int i=0; i<nr_params-1; i++) {
+                    printf("%llu, ",param_regs[i]);
+                }
+                printf("%llu)\n",param_regs[nr_params-1]);
+            } else {
+                printf(")\n");
+            }
+            run_idx++;
+            orig_return_addr = return_addr;
+            ret_data = ptrace(PTRACE_PEEKTEXT, child_pid, (void*)orig_return_addr, NULL);
+            ret_data_trap = (ret_data & 0xFFFFFFFFFFFFFF00) | 0xCC;
+            ptrace(PTRACE_POKETEXT, child_pid, (void*)orig_return_addr, (void*)ret_data_trap);
+        }
+        // Restore the original instruction and single step
+        ptrace(PTRACE_POKETEXT, child_pid, (void*)addr, (void*)data);
+        ptrace(PTRACE_SETREGS, child_pid, NULL, &regs);
+        ptrace(PTRACE_SINGLESTEP, child_pid, NULL, NULL);
+        wait(&wait_status);
+        ptrace(PTRACE_POKETEXT, child_pid, (void*)addr, (void*)data_trap);
+        ptrace(PTRACE_CONT, child_pid, NULL, NULL);
+        wait(&wait_status);
+
+        } else if(orig_return_addr != 0 && regs.rip == orig_return_addr) {
+            printf("PRF::  call function returned with %llu\n", regs.rax);
+            ptrace(PTRACE_POKETEXT, child_pid, (void*)orig_return_addr, (void*)ret_data);
+            break;
+        }
+    }
+}
+
+int main(int argc, char* const argv[])
+{
+    if (argc < 4) {
+        printf("usage: <sym_name> <number of input params> <elf_path> "
+               "[optional input params for elf file]\n");
+        return 0;
+    }
+
+    const char* sym_name  = argv[1];
+    int nr_params         = strtol(argv[2], NULL, 10);
+    const char* file_name = argv[3];
+
+    // Put file content into memory for parsing
+    void* file_content = get_elf_content(sym_name, file_name);
+
+    // Find the symbol address
+    unsigned long addr = parse_elf(sym_name, file_content);
+
+    // Free the allocated memory
+    free(file_content);
+
+    // TODO: check if symbol was found
+    if (addr == 0) {
+        printf("PRF:: symbol not found\n");
+        return 1;
+    }
+    printf("PRF:: symbol address is 0x%lx\n\n",addr);
+
+    // Launch the target program
+    pid_t child_pid = run_target(argv + 3);
+
+    // Run the tracer
+    run_tracer(child_pid, addr, nr_params);
+
+    return 0;
+}
---
+++ tests_for_students/run_tests.sh
@@ -0,0 +1,128 @@
+#!/bin/bash
+
+cd ./tests
+gcc -std=c99 *.c -o prf
+if [ -f "prf" ]; then
+	timeout 20s ./prf foo 2 program1.out > studentout.txt
+	if [ $? -eq 0 ] && diff "out1.txt" studentout.txt >/dev/null 2>&1; then
+		echo "Test 1: PASS"
+	else
+		echo "Test 1: FAIL"
+	fi
+
+	timeout 20s ./prf rec_foo 1 program2.out > studentout.txt
+	if [ $? -eq 0 ] && diff "out2.txt" studentout.txt >/dev/null 2>&1; then
+		echo "Test 2: PASS"
+	else
+		echo "Test 2: FAIL"
+	fi
+
+	timeout 20s ./prf rec_bar 1 program3.out > studentout.txt
+	if [ $? -eq 0 ] && diff "out3.txt" studentout.txt >/dev/null 2>&1; then
+		echo "Test 3: PASS"
+	else
+		echo "Test 3: FAIL"
+	fi
+
+	timeout 20s ./prf food 1 program4.out > studentout.txt
+	if [ $? -eq 0 ] && diff "out4.txt" studentout.txt >/dev/null 2>&1; then
+		echo "Test 4: PASS"
+	else
+		echo "Test 4: FAIL"
+	fi
+
+	timeout 20s ./prf food 1 program5.out > studentout.txt
+	if [ $? -eq 0 ] && diff "out5.txt" studentout.txt >/dev/null 2>&1; then
+		echo "Test 5: PASS"
+	else
+		echo "Test 5: FAIL"
+	fi
+
+	timeout 20s ./prf food 1 program6.out > studentout.txt
+	if [ $? -eq 0 ] && diff "out6.txt" studentout.txt >/dev/null 2>&1; then
+		echo "Test 6: PASS"
+	else
+		echo "Test 6: FAIL"
+	fi
+
+	timeout 20s ./prf foo 2 program7.out > studentout.txt
+	if [ $? -eq 0 ] && diff "out7.txt" studentout.txt >/dev/null 2>&1; then
+		echo "Test 7: PASS"
+	else
+		echo "Test 7: FAIL"
+	fi
+
+	timeout 20s ./prf foo 2 program8.out > studentout.txt
+	if [ $? -eq 0 ] && diff "out8.txt" studentout.txt >/dev/null 2>&1; then
+		echo "Test 8: PASS"
+	else
+		echo "Test 8: FAIL"
+	fi
+
+	timeout 20s ./prf foo 2 program9.out > studentout.txt
+	if [ $? -eq 0 ] && diff "out9.txt" studentout.txt >/dev/null 2>&1; then
+		echo "Test 9: PASS"
+	else
+		echo "Test 9: FAIL"
+	fi
+
+	timeout 20s ./prf foo 1 program10.out > studentout.txt
+	if [ $? -eq 0 ] && diff "out10.txt" studentout.txt >/dev/null 2>&1; then
+		echo "Test 10: PASS"
+	else
+		echo "Test 10: FAIL"
+	fi
+
+	timeout 20s ./prf foo 2 program11.out R > studentout.txt
+	if [ $? -eq 0 ] && diff "out11.txt" studentout.txt >/dev/null 2>&1; then
+		echo "Test 11: PASS"
+	else
+		echo "Test 11: FAIL"
+	fi
+
+	timeout 20s ./prf nonexistent 1 program1.out > studentout.txt
+	if [ $? -eq 1 ] && diff "out12.txt" studentout.txt >/dev/null 2>&1; then
+		echo "Test 12: PASS"
+	else
+		echo "Test 12: FAIL"
+	fi
+
+	timeout 20s ./prf zero_params_func 0 program13.out > studentout.txt
+	if [ $? -eq 0 ] && diff "out13.txt" studentout.txt >/dev/null 2>&1; then
+		echo "Test 13: PASS"
+	else
+		echo "Test 13: FAIL"
+	fi
+
+	timeout 20s ./prf six_params_func 6 program14.out > studentout.txt
+	if [ $? -eq 0 ] && diff "out14.txt" studentout.txt >/dev/null 2>&1; then
+		echo "Test 14: PASS"
+	else
+		echo "Test 14: FAIL"
+	fi
+
+	timeout 20s ./prf fib 1 program15.out > studentout.txt
+	if [ $? -eq 0 ] && diff "out15.txt" studentout.txt >/dev/null 2>&1; then
+		echo "Test 15: PASS"
+	else
+		echo "Test 15: FAIL"
+	fi
+
+	timeout 20s ./prf deep_rec_func 1 program16.out > studentout.txt
+	if [ $? -eq 0 ] && diff "out16.txt" studentout.txt >/dev/null 2>&1; then
+		echo "Test 16: PASS"
+	else
+		echo "Test 16: FAIL"
+	fi
+
+	timeout 20s ./prf fib 1 program17.out > studentout.txt
+	if [ $? -eq 0 ] && diff "out17.txt" studentout.txt >/dev/null 2>&1; then
+		echo "Test 17: PASS"
+	else
+		echo "Test 17: FAIL"
+	fi
+else
+	echo "Compilation error - cannot create debugger file"
+fi
+
+rm -f "prf" "studentout.txt"
